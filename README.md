bebfc9f5f6a2fb20ebf6126f37671fed0c2a2092
Denver Dias <www.denver@outlook.com>
2020-08-10 22:24:34 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Async Maps
+
+An async version of JavaScript Maps.
+
+## Install
+```
+npm install @dnvr/async-maps
+```
+
+## How it works
+
+Async Maps work similar to JavaScript Maps and is infact based on it.
+
+It is a bit more restricive than regular Maps in that it offers no options to check for keys or delete or clear the Map. A value can only be set once for a given key and subsequent uses of the `set` method would be ineffective.
+
+It allows using primitives as keys unlike with WeakMaps.
+
+With Async Maps `get`ting the value corresponding to a key returns a Promise that resolves to the value when it is `set`.
+
+## Usage
+```TS
+import AsyncMap from '@dnvr/async-maps'
+
+const map = new AsyncMap
+
+map.get( 'info' ).then( ( information ) => {
+  console.log( information )
+})
+
+void async function () {
+  let value = await map.get( 'value' )
+
+  console.log( value )
+}()
+
+map.set( 'info', 'Hello World!!!' )
+map.set( 'value', 1 )
+```
+
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+const key2promise: unique symbol = Symbol()
+const promise2resolver: unique symbol = Symbol()
+
+class AsyncMap<K = any, V = any> {
+  private [ key2promise ]: Map<K, Promise<V>>
+  private [ promise2resolver ]: WeakMap<Promise<V>, ( value: V ) => void>
+
+  public constructor ( iterable?: Iterable<[ K, V ]> ) {
+    this[ key2promise ] = new Map
+    this[ promise2resolver ] = new WeakMap
+
+    if ( 'undefined' !== typeof iterable ) {
+      for ( let entry of iterable ) {
+        let [ key, value ] = entry
+
+        this.set( key, value )
+      }
+    }
+  }
+
+  public get ( key: K ): Promise<V> {
+    if ( this[ key2promise ].has( key ) ) {
+      return this[ key2promise ].get( key ) as Promise<V>
+    }
+    else {
+      let resolve: ( value: V ) => void
+      let promise = new Promise( function ( resolution: ( value: V ) => void ) {
+        resolve = resolution
+      } )
+
+
+      this[ key2promise ].set( key, promise )
+
+      // @ts-expect-error
+      this[ promise2resolver ].set( promise, resolve )
+
+      return promise
+    }
+  }
+
+  public set ( key: K, value: V ) {
+    this[ promise2resolver ].get( this.get( key ) )
+  }
+}
+
+export default AsyncMap
+
+let k = new AsyncMap
